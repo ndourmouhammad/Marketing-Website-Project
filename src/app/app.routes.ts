29fb837627ad6fceb9bd436e51d07c808e3f9b8a
Mouhammad ndour <ndourmouhammad15@gmail.com>
2025-02-07 00:31:40 +0000

    Initialisation et mise en place du projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { FeatureComponent } from './components/feature/feature.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'features', component: FeatureComponent },
+  { path: 'landing', component: LandingPageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: '**', component: NotFoundComponent },
+];
